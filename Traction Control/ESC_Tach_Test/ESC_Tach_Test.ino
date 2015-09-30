86bf9b370482dec2e45e3fba5cd344fb427456ea
wzschoche <wzschoche@csu.fullerton.edu>
2015-09-29 18:48:07 -0700

    Single ESC Test with Loop Logger
    ESC_Test rewritten and relabeled to Single_ESC_Test

--- Traction Control/ESC_Tach_Test/ESC_Tach_Test.ino
+++ Traction Control/ESC_Tach_Test/ESC_Tach_Test.ino
@@ -14,24 +14,42 @@ Servo motor3;
 Servo motor4;
 
 uint32_t ulTimeStart;
+uint32_t cycles;
 
 void setup() {
+  //initialize Serial output
+  Serial.begin(9600);
+  
+  //attach output pins
   motor1.attach(ESC1_OUT_PIN);
   motor2.attach(ESC2_OUT_PIN);
   motor3.attach(ESC3_OUT_PIN);
   motor4.attach(ESC4_OUT_PIN);
 
+  //Calibrate ESCs with idle signal
   setSpeedAll(CAL_SIGNAL);
 
+  //initialize timer
   ulTimeStart = micros();
+
+  //initiliaze cycle counter
+  cycles = 0;
 }
 
 void loop() {
-  if ((micros() - ulTimeStart) <= 2000000) {
+  //Ensure ESCs are calibrated by sending idles signal for a
+  //minimum of 4 seconds.
+  if ((micros() - ulTimeStart) <= 4000000) {
     setSpeedAll(CAL_SIGNAL);
   }
   else {
     setSpeedAll(TEST_SIGNAL);
+  }
+  //count cycles
+  ++cycles;
+
+  if(cycles % 1000 == 0) {
+    Serial.println(cycles);
   }
 }
 
